d976e8b0a4b83aeb7c03a656c67410b1065ddedc
raju <nethunuriraju4@gmail.com>
2026-08-27 19:21:04 +0530

    added DataTypes

---
+++ java/01-java-Basics/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        boolean student = true;
+        char grade = 'A';
+        float percentage = 85.5f;
+        double salary = 25000.50;
+
+        System.out.println("Integer: " + age);
+        System.out.println("Boolean: " + student);
+        System.out.println("Character: " + grade);
+        System.out.println("Float: " + percentage);
+        System.out.println("Double: " + salary);
+    }
+}
