357d29d9f0ab7c82536856a8edafba51789218e0
raju <nethunuriraju4@gmail.com>
2026-08-27 19:00:30 +0530

    added Q3

---
+++ java/01-java-Basics/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment.
+         * It can contain multiple lines.
+         */
+
+        /**
+         * This is a documentation comment.
+         * It is used to provide documentation.
+         */
+
+        System.out.println("Comments in Java");
+    }
+}
