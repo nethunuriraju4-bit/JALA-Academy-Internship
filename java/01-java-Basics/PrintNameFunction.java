3adea70ba35838a77d8abfd16810caa0ee5427d6
raju <nethunuriraju4@gmail.com>
2026-08-27 19:48:43 +0530

    added program:print name and function call from main method

---
+++ java/01-java-Basics/PrintNameFunction.java
@@ -0,0 +1,10 @@
+public class PrintNameFunction {
+
+    static void printName() {
+        System.out.println("raju yadav");
+    }
+
+    public static void main(String[] args) {
+        printName();
+    }
+}
