8999f4f580a1dfc481e836cc183976d9416072b7
raju <nethunuriraju4@gmail.com>
2026-08-27 18:12:52 +0530

    Added java basics Assignment 1

---
+++ java/01-java-Basics/Object.java
@@ -0,0 +1,11 @@
+public class Object {
+
+    void display() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        obj.display();
+    }
+}
---
+++ java/01-java-Basics/README.md
@@ -0,0 +1,13 @@
+# JALA Academy - Java Basics
+
+## Question 1
+
+How to create a class, object, method and its signature.
+
+### Solution
+
+The program is available in Object.java.
+
+### Output
+
+Hello
