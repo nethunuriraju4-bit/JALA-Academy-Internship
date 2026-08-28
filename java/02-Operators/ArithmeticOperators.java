d86b725dd690e3dee7d22260efb53c0b3a8e1b97
raju <nethunuriraju4@gmail.com>
2026-08-28 10:02:30 +0530

    added Arithmetic operators program

---
+++ java/02-Operators/ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators {
+
+    static void arithmetic(int a, int b) {
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+
+    public static void main(String[] args) {
+        arithmetic(20, 10);
+    }
+}
