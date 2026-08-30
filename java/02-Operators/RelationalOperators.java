71c265f8ed1cddab6e32423f842263373daa0944
raju <nethunuriraju4@gmail.com>
2026-08-30 18:54:22 +0530

    added program for Relational operators

---
+++ java/02-Operators/RelationalOperators.java
@@ -0,0 +1,13 @@
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Less than: " + (a < b));
+        System.out.println("Less than or equal to: " + (a <= b));
+        System.out.println("Greater than: " + (a > b));
+        System.out.println("Greater than or equal to: " + (a >= b));
+    }
+}
