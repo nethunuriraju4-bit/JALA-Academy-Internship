635137ac4347648c251c829fa7cdf52d9b03ced9
raju <nethunuriraju4@gmail.com>
2026-08-30 19:36:01 +0530

    added program to print smaller and larger numbers

---
+++ java/02-Operators/SmallerLarger.java
@@ -0,0 +1,16 @@
+public class SmallerLarger {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if (a < b) {
+            System.out.println("Smaller number: " + a);
+            System.out.println("Larger number: " + b);
+        } else {
+            System.out.println("Smaller number: " + b);
+            System.out.println("Larger number: " + a);
+        }
+    }
+}
