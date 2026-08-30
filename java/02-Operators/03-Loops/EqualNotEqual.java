c8da56ba3c2df0d71f54de80c38aae6c905c2eac
raju <nethunuriraju4@gmail.com>
2026-08-30 19:58:14 +0530

    added program to check equal and not equal conditions

---
+++ java/02-Operators/03-Loops/EqualNotEqual.java
@@ -0,0 +1,11 @@
+public class EqualNotEqual {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Equal: " + (a == b));
+        System.out.println("Not Equal: " + (a != b));
+    }
+}
