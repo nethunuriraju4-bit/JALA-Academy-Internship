b3b8d8584d467357a9709e575472493ea6a90b35
raju <nethunuriraju4@gmail.com>
2026-08-30 18:42:29 +0530

    added program for checking equal condition

---
+++ java/02-Operators/CheckEqual.java
@@ -0,0 +1,13 @@
+public class CheckEqual {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        if (a == b) {
+            System.out.println("Both numbers are equal");
+        } else {
+            System.out.println("Both numbers are not equal");
+        }
+    }
+}
