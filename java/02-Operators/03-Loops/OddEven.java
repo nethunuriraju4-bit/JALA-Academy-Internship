27bd789b9dbd3d13e71912e248fdc74c81e23efe
raju <nethunuriraju4@gmail.com>
2026-08-31 10:11:27 +0530

    program to print odd and even numbers

---
+++ java/02-Operators/03-Loops/OddEven.java
@@ -0,0 +1,21 @@
+public class OddEven {
+
+    public static void main(String[] args) {
+
+        System.out.println("Even numbers:");
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Odd numbers:");
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
