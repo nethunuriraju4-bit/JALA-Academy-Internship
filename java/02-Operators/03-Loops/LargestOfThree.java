8787e2c8f35c105f0120b935fe3f521726705e13
raju <nethunuriraju4@gmail.com>
2026-08-31 10:17:50 +0530

    added program to print largest number among three numbers

---
+++ java/02-Operators/03-Loops/LargestOfThree.java
@@ -0,0 +1,17 @@
+public  class LargestOfThree {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        if (a >= b && a >= c) {
+            System.out.println("Largest number: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest number: " + b);
+        } else {
+            System.out.println("Largest number: " + c);
+        }
+    }
+}
