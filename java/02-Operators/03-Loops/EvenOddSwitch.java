8bc051e4ea676022529625ad33ed1b2b4abc130b
raju <nethunuriraju4@gmail.com>
2026-08-31 13:07:59 +0530

    progeram for evenodd number

---
+++ java/02-Operators/03-Loops/EvenOddSwitch.java
@@ -0,0 +1,17 @@
+public class EvenOddSwitch {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        switch (number % 2) {
+            case 0:
+                System.out.println("Even Number");
+                break;
+
+            case 1:
+                System.out.println("Odd Number");
+                break;
+        }
+    }
+}
