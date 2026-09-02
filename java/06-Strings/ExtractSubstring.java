a1f7b358255a5944d698694f397540fa6f6fc11a
raju <nethunuriraju4@gmail.com>
2026-09-02 17:40:36 +0530

    extract substring

---
+++ java/06-Strings/ExtractSubstring.java
@@ -0,0 +1,11 @@
+public class ExtractSubstring {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        String result = str.substring(0, 5);
+
+        System.out.println("Extracted String: " + result);
+    }
+}
