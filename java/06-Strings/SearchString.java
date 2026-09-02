b4cc1d48c301665917007da307abb4b10051818b
raju <nethunuriraju4@gmail.com>
2026-09-02 17:42:49 +0530

    search string

---
+++ java/06-Strings/SearchString.java
@@ -0,0 +1,11 @@
+public class SearchString {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        int index = str.indexOf("World");
+
+        System.out.println("Index of World: " + index);
+    }
+}
