67e210ba2d742cb0b59c022f35cdcf23dae6b776
raju <nethunuriraju4@gmail.com>
2026-09-02 17:46:27 +0530

    compare to strings

---
+++ java/06-Strings/CompareToStrings.java
@@ -0,0 +1,12 @@
+public class CompareToStrings {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2 = "World";
+
+        int result = str1.compareTo(str2);
+
+        System.out.println("Comparison result: " + result);
+    }
+}
