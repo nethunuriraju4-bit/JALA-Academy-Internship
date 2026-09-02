555f4433bc28adcd05dc403a04ebcf829bff6f20
raju <nethunuriraju4@gmail.com>
2026-09-02 17:31:38 +0530

    concatenate of strings

---
+++ java/06-Strings/ConcatenateStrings.java
@@ -0,0 +1,12 @@
+public class ConcatenateStrings {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2 = "World";
+
+        String result = str1 + " " + str2;
+
+        System.out.println("Concatenated String: " + result);
+    }
+}
