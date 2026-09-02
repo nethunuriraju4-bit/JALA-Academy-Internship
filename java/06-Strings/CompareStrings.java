f2abfa804a5ab9662a0c6a5fdaff0aae7b39b620
raju <nethunuriraju4@gmail.com>
2026-09-02 17:44:36 +0530

    comparing of strings

---
+++ java/06-Strings/CompareStrings.java
@@ -0,0 +1,14 @@
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        if (str1.equals(str2)) {
+            System.out.println("Both strings are equal");
+        } else {
+            System.out.println("Both strings are not equal");
+        }
+    }
+}
