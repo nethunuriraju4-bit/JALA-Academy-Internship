2f8ae25af1f45099cc641f26dcd7b3a41f854d44
raju <nethunuriraju4@gmail.com>
2026-09-02 17:37:23 +0530

    String length

---
+++ java/06-Strings/StringLength.java
@@ -0,0 +1,11 @@
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        int length = str.length();
+
+        System.out.println("Length of the string: " + length);
+    }
+}
